619636f34744c102ca8af7dde5d3702322130f38
Harshit jain <harshitjain0904@gmail.com>
2026-06-15 16:32:50 +0530

    Add Strings Program

---
+++ Strings/CheckIfStringIsPalindromeOrNot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter a Strng: ";
+    cin>>s;
+    string rev = "";
+    for(int i = s.length()-1;i>=0;i--){
+        rev+= s[i];
+    }
+    if(rev==s){
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not a Palindrpme";
+    }
+}
